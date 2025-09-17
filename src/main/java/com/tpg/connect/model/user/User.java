c0a5c9c3807decd4b5c22738993bcfc3ec796a2e
WillG9999 <Will_graham@live.com>
2025-09-17 21:55:25 +0100

    feat: inital commit

---
+++ src/main/java/com/tpg/connect/model/user/User.java
@@ -0,0 +1,17 @@
+package com.tpg.connect.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private boolean active;
+}
---
+++ src/main/java/com/tpg/connect/model/user/UserContext.java
@@ -0,0 +1,38 @@
+package com.tpg.connect.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserContext {
+    private String userId;
+    private String username;
+    private String email;
+    private String role;
+    private boolean authenticated;
+
+    public static UserContext fromUser(User user) {
+        return new UserContext(
+            user.getId(),
+            user.getUsername(),
+            user.getEmail(),
+            user.getRole(),
+            true
+        );
+    }
+
+    public boolean hasRole(String role) {
+        return this.role != null && this.role.equals(role);
+    }
+
+    public boolean isAdmin() {
+        return hasRole("ADMIN");
+    }
+
+    public boolean isUser() {
+        return hasRole("USER");
+    }
+}
